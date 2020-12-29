5f43df4386ae1d96119d664a4b0c7520c4adeef9
Shubha VB <shubha.dec03@gmail.com>
2020-12-29 16:09:01 +0530

    changed the get_database() to get_default_database()

--- common/database.py
+++ common/database.py
@@ -4,7 +4,7 @@ from typing import Dict
 
 class Database:
     URI = os.environ.get("MONGO_URI")
-    DATABASE = pymongo.MongoClient(URI).get_database()
+    DATABASE = pymongo.MongoClient(URI).get_default_database()
 
     @staticmethod
     def insert(collection: str, data: Dict):
